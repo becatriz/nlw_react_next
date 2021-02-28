38b8bbf8afcb6d62d1cebb1012af01b01ca3648c
Rebeca Lopes <becatriz7@gmail.com>
2021-02-28 15:17:00 -0400

    feat: compartilhar twitter

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -1,23 +1,47 @@
-import { useContext } from "react"
-import { ChallengesContext } from "../contexts/ChallengesContexts"
-import styles from "../styles/components/LevelUpModal.module.css"
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContexts";
+import styles from "../styles/components/LevelUpModal.module.css";
+
+export function LevelUpModal() {
+  const {
+    level,
+    challengesCompleted,
+    currentExperience,
+    closeLevelUpModal,
+  } = useContext(ChallengesContext);
 
 
-export function LevelUpModal(){
-    const { level, closeLevelUpModal  } = useContext(ChallengesContext)
-
-    return (
-        <div className={styles.overlay}>
-            <div className={styles.container}>
-                <header>{level}</header>
-
-                <strong>Parabéns</strong>
-                <p>Você alcançou um novo nível</p>
-
-                <button type="button" onClick={closeLevelUpModal} >
-                    <img src="/icons/close.svg" alt="Fechar Modal"/>
-                </button>
-            </div>
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <div className={styles.avancedLevelUpModal}>
+          <header>{level}</header>
+          <strong>Avancei para o próximo nível</strong>
         </div>
-    )
+
+        <div className={styles.challengesCompleted}>
+          <span>Desafios</span>
+          <p>{challengesCompleted.toString()} completados</p>
+          <span>Experiência</span>
+          <p>{currentExperience.toString()} xp</p>
+        </div>
+
+        <button
+          className={styles.closeModal}
+          type="button"
+          onClick={closeLevelUpModal}
+        >
+          <img src="/icons/close.svg" alt="Fechar Modal" />
+        </button>
+        <a
+          className={styles.sharedOnTwitter}
+          href="https://twitter.com/intent/tweet?url=https://movieit-nlw4-8gxjtbzty-becatriz.vercel.app//&amp;text=Yupi!!! Completei mais um desafio&amp;"
+          
+        >
+          Compartilhar no Twitter
+          <img src="/icons/twitter.svg" alt="Logo Twitter" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,28 @@
-import Document, { Html, Head, NextScript, Main } from "next/document"
+import Document, { Html, Head, NextScript, Main } from "next/document";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="shortcut icon" href="favicon.png" type="image/png" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-                        rel="stylesheet"
-                    />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta property="og:title" content="Moveit" key="title" />
+          <meta
+            property="og:image"
+            content="/logo-full_2.svg"
+            key="title"
+          />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
